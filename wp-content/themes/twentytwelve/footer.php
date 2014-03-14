0c6f2bd81bce2c6e2de0cf4c6e4ba3fd834fcedd
Ubuntu <lauradhamilton@gmail.com>
2014-03-14 19:16:36 +0000

    Upgrading the twentytwelve theme

--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The template for displaying the footer
  *
- * Contains footer content and the closing of the
- * #main and #page div elements.
+ * Contains footer content and the closing of the #main and #page div elements.
  *
  * @package WordPress
  * @subpackage Twenty_Twelve
@@ -14,27 +13,11 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
-			©2013-<?php echo date("Y"); ?> Additive Analytics, LLC. All rights reserved.
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-44774227-1', 'additiveanalytics.com');
-  ga('send', 'pageview');
-
-</script>
-
 </body>
 </html>
-
-<?php
-  ob_flush();
-?>
--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -1,10 +1,6 @@
 <?php
-//Replace hardcoded domain name in absolute links
-  ob_start('fix_links');
-?>
-
-<?php
-/*The Header for our theme.
+/**
+ * The Header template for our theme
  *
  * Displays all of the <head> section and everything up till <div id="main">
  *
@@ -12,9 +8,7 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
@@ -51,9 +45,8 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+		<?php if ( get_header_image() ) : ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
 		<?php endif; ?>
 	</header><!-- #masthead -->
 
